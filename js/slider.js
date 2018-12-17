5136b29261dd3d6925777f1c655490b40e75f34b
alex <stwork87@gmail.com>
2018-12-17 22:38:52 +0300

    draft for style

--- js/slider.js
+++ js/slider.js
@@ -1,30 +1,30 @@
 "use strict";
 $(document).ready(function($) {
   $('.recalls__control--next-slide').click(function (){
-    var currentSlide = $('.recalls__slide--current');
+    var currentSlide = $('.recalls__slide-wrapper--current');
     var currentSlideIndex = currentSlide.index();
     var nextSlideIndex = currentSlideIndex + 1;
-    var nextSlide = $(".recalls__slide").eq(nextSlideIndex);
+    var nextSlide = $(".recalls__slide-wrapper").eq(nextSlideIndex);
     currentSlide.fadeOut(1000);
-    currentSlide.removeClass('recalls__slide--current');
+    currentSlide.removeClass('recalls__slide-wrapper--current');
 
-    if (nextSlideIndex == ($(".recalls__slide:last").index()+1)){
-      $('.recalls__slide').eq(0).fadeIn(1000);
-      $('.recalls__slide').addClass('recalls__slide--current');
+    if (nextSlideIndex == ($(".recalls__slide-wrapper:last").index()+1)){
+      $('.recalls__slide-wrapper').eq(0).fadeIn(1000);
+      $('.recalls__slide-wrapper').addClass('recalls__slide-wrapper--current');
     }
     else{
       nextSlide.fadeIn(1000);
-      nextSlide.addClass('recalls__slide--current');
+      nextSlide.addClass('recalls__slide-wrapper--current');
     }
   });
   $('.recalls__control--prev-slide').click(function() {
-    var currentSlide = $('.recalls__slide--current');
+    var currentSlide = $('.recalls__slide-wrapper--current');
     var currentSlideIndex = currentSlide.index();
     var prevSlideIndex = currentSlideIndex - 1;
-    var prevSlide = $(".recalls__slide").eq(prevSlideIndex);
+    var prevSlide = $(".recalls__slide-wrapper").eq(prevSlideIndex);
     currentSlide.fadeOut(1000);
-    currentSlide.removeClass('recalls__slide--current');
+    currentSlide.removeClass('recalls__slide-wrapper--current');
     prevSlide.fadeIn(1000);
-    prevSlide.addClass('recalls__slide--current');
+    prevSlide.addClass('recalls__slide-wrapper--current');
   });
 });
